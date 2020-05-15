af2caa888be6de6fce7059e001eae44e5074a0a2
marco sacco <marcosacco74@hotmail.com>
2020-05-15 22:40:09 +0100

    main commit

---
+++ src/components/auth/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Welcome() {
+  return (
+    <section className="section auth">
+      <div className="container">
+        <h1>Welcome!</h1>
+        <p>You have successfully registered a new account.</p>
+        <p>Please click link to verify account.</p>
+      </div>
+    </section>
+  )
+}
