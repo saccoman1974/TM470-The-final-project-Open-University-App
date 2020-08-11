a65674d04842b29e393e44c7974e98fd38c82080
marco sacco <marcosacco74@hotmail.com>
2020-08-11 16:21:08 +0100

    media queries set for mobile viewing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="footer">
       <div className="content has-text-centered">
         <p>
-           Marco Sacco The Open University TM470 2020. The original source code from Hexal is licensed MIT. The website content is licensed CC BY NC SA 4.0.
+           Marco Sacco The Open University TM470 2020. The original source code from CloudPath is licensed MIT. The website content is licensed CC BY NC SA 4.0.
         </p>
       </div>
     </footer>
